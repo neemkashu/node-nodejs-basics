f645c9959070f7a2d90f9d6f368681e1d5c9bc5b
neemkashu <sparjinkust@mail.ru>
2024-01-23 23:52:56 +0300

    fit: task create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from "node:fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code uR4mU7rG7jxQ
+  // implement function that creates new file fresh.txt with content
+  // I am fresh and young
+  // inside of the files folder
+  // (if file already exists Error with message FS operation failed must be thrown)
+
+  try {
+    await writeFile("fresh.txt", "I am fresh and young", { flag: "wx" });
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
