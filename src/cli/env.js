70d53a9a7570915816f7f1d202d8fd2d1d4aed8a
neemkashu <sparjinkust@mail.ru>
2024-01-24 13:38:00 +0300

    task 2

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  // implement function that parses environment variables
+  // with prefix RSS_ and prints them to the console
+  // in the format RSS_name1=value1; RSS_name2=value2
+  const envs = process.env;
+  const result = Object.entries(envs)
+    .filter(([key]) => {
+      return key.startsWith("RSS_");
+    })
+    .reduce((accum, [key, value]) => {
+      accum.push(`${key}=${value}`);
+      return accum;
+    }, [])
+    .join("; ");
+  console.log(result);
 };
 
 parseEnv();
