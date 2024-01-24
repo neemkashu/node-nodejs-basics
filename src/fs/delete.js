a26f102868d05d3b96d1d7e222c5452dce608c51
neemkashu <sparjinkust@mail.ru>
2024-01-24 12:01:49 +0300

    complete fs

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { rm } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  //  implement function that deletes file fileToRemove.txt
+  // (if there's no file fileToRemove.txt Error with message
+  // FS operation failed must be thrown)
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const source = path.join(dirname, "files", "fresh.txt");
+  try {
+    await rm(source);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
