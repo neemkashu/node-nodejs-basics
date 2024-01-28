8b04a7a2b62171548ae3a01fb0de2ef5a676f3f9
neemkashu <sparjinkust@mail.ru>
2024-01-29 01:54:57 +0300

    feat: workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const CORES_AMOUNT = cpus().length;
+  const MIN_FIBONACCI_ARGUMENT = 10;
+  const MAX_FIBONACCI_ARGUMENT = 10 + CORES_AMOUNT;
+  const __dirname = import.meta.dirname;
+  const workerPath = path.join(__dirname, "worker.js");
+
+  function runService(workerData) {
+    // if you know a better way please connect me
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, { workerData });
+      worker.on("message", resolve);
+      worker.on("error", reject);
+    });
+  }
+
+  const results = [];
+  for (let i = MIN_FIBONACCI_ARGUMENT; i < MAX_FIBONACCI_ARGUMENT; i++) {
+    results.push(runService(i));
+  }
+  const workersData = await Promise.allSettled(results);
+
+  const dataConverted = workersData.map((elem) => {
+    const { status, value = null } = elem;
+    const statusConverted = convertStatus(status);
+    return {
+      status: statusConverted,
+      data: value,
+    };
+  });
+
+  console.log(dataConverted);
 };
 
 await performCalculations();
+
+function convertStatus(status) {
+  const map = {
+    rejected: "error",
+    fulfilled: "resolved",
+  };
+  return map[status];
+}
