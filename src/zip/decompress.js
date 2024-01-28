8850a38999ec5d158a2aab851ba947bdfda5d19e
neemkashu <sparjinkust@mail.ru>
2024-01-29 02:14:44 +0300

    feat: zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { pipeline } from "node:stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+import path from "node:path";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const file = path.join(__dirname, "files", "fileToCompress.txt");
+  const archive = path.join(__dirname, "files", "archive.gz");
+
+  await pipeline(
+    createReadStream(archive),
+    createUnzip(),
+    createWriteStream(file)
+  );
 };
 
 await decompress();
